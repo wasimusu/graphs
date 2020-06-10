e2ed992ee3ba0973bfb8e3d4a0045ecf8bc12469
wasimusu <wasim.khan@aggiemail.usu.edu>
2020-06-10 12:07:51 -0600

    updated traversal for undirected graphs

--- readme.md
+++ readme.md
@@ -1,7 +1,8 @@
 ## Graphs
-An open-source modern graph library built for speed in C++.
+An open-source modern graph library built in C++ for exploring graphs.
 
 ### Features
+* Ease of use.
 * Easily discoverable member functions
 * Lightweight and generic graph representation
 * Inputs are ids of nodes. Or let the nodes be decided automatically by the data structure
@@ -20,10 +21,14 @@ cmake ..
 make
 ```
 
-### Algorithms (To be published by June 14, 2020)
-* Three different representation of graph - adjacency matrix, adjacency list, edge list
+### Data Structures for graph representation
 The value of a graph library lies in making it easy to construct and analyze graphs. 
-* Traversals (BFS, DFS)
+* adjacencyList
+* adjacencyMatrix
+* edgeList
+
+### Algorithms (To be published by June 14, 2020)
+* Traversals (BFS, DFS, Level Order Traversal)
 * Topological Sort
 * Print graphs for easy debugging. Multiple formats for printing graphs
 * Prim's Minimum Spanning Tree
@@ -37,14 +42,19 @@ The value of a graph library lies in making it easy to construct and analyze gra
 * Tarjan's Strongly Connected Components
 * Bellman Ford Algorithm
 * Is the graph bipartite?
-
+* Serialization/deserialization
 
 ### Upcoming features/development
 * Benchmarks
 * Google Tests
 
 ### Questions that can be solved by graphs
-* Find dependency between files.
-* What is the degree of distance between two people?
+* Find dependency between files. Do topological sort on a graph representing files.
+* What is the degree of distance between two people? 
+Just find the shortest distance between two people (the edges should have unit weight).
 * How many connected components are there in the graph?
 * Is there circular dependency in the graph? Or are there loops in the graph?
+
+### Goals
+* Double the speed of the whole library.
+* Reduce memory consumption
--- src/traverse.h
+++ src/traverse.h
@@ -13,6 +13,7 @@
 
 /**
  * @brief Implements iterative algorithm for breadth first search for a graph.
+ * For undirected graphs we have keep track of visited nodes.
  *
  * */
 void bfs(graph &graph, int source_node, std::vector<int> &result) {
@@ -21,11 +22,19 @@ void bfs(graph &graph, int source_node, std::vector<int> &result) {
     std::queue<int> queue;
     queue.push(source_node);
 
+    int n = graph.get_num_nodes();
+    std::vector<bool> visited(n, false);
+
     while (!queue.empty()) {
         auto node = queue.front();
         queue.pop();
+
+        if(visited[node]) continue;
+        visited[node] = true;
+
         result.push_back(node);
         for (const int child: edges[node]) {
+            if (visited[child]) continue;
             queue.push(child);
         }
     }
@@ -33,6 +42,7 @@ void bfs(graph &graph, int source_node, std::vector<int> &result) {
 
 /**
  * @brief Implements iterative algorithm for depth first search for a graph.
+ * Keep track of the visited nodes in graphs. It's not required to keep track of visited nodes in directed graphs
  *
  * */
 void dfs(graph &graph, int source_node, std::vector<int> &result) {
@@ -40,11 +50,19 @@ void dfs(graph &graph, int source_node, std::vector<int> &result) {
     std::vector<int> stack;
     stack.push_back(source_node);
 
+    int n = graph.get_num_nodes();
+    std::vector<bool> visited(n, false);
+
     while (!stack.empty()) {
         auto node = stack.back();
         stack.pop_back();
+
+        if(visited[node]) continue;
+        visited[node] = true;
+
         result.push_back(node);
         for (const int child: edges[node]) {
+            if (visited[child]) continue;
             stack.push_back(child);
         }
     }
@@ -59,10 +77,18 @@ void level_order_traversal(graph &graph, int source_node, std::vector<std::vecto
     std::vector<int> parent = {source_node};
     std::vector<int> children;
 
+    int n = graph.get_num_nodes();
+    std::vector<bool> visited(n, false);
+
     while (!parent.empty()) {
 
         for (int node: parent) {
+
+            if(visited[node]) continue;
+            visited[node] = true;
+
             for (int child: edges[node]) {
+                if (visited[child]) continue;
                 children.push_back(child);
             }
         }
