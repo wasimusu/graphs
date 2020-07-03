f6fe81b004c036dc7d689a379a51738c8beee205
wasimusu <wasim.khan@aggiemail.usu.edu>
2020-07-03 16:13:01 -0600

    upgrade sphinx

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -20,9 +20,11 @@ read_the_docs_build = os.environ.get('READTHEDOCS', None) == 'True'
 
 curr_dir = os.path.abspath('.')
 import sphinx as s
+
 print("Curr version of sphinx: ", s.__version__)
 if read_the_docs_build:
     os.chdir('..')
+    subprocess.call('pip install sphinx --upgrade', shell=True)
     subprocess.call('doxygen', shell=True)
     os.chdir(curr_dir)
     print("Build was built ", os.listdir("../build"))
